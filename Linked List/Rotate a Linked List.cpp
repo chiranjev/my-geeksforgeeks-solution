174832919aa5ad781110e1989af9c438928c3047
chiranjev <koulsahil04321@gmail.com>
2018-08-04 22:59:53 +0530

    4/8/18

---
+++ Linked List/Rotate a Linked List.cpp
@@ -0,0 +1,99 @@
+{
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node* next;
+}*head;
+void rotate(struct node **head_ref, int k);
+  void insert()
+{
+    int n,i,value;
+    struct node *temp;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&value);
+        if(i==0)
+        {
+            head=(struct node *) malloc( sizeof(struct node) );
+            head->data=value;
+            head->next=NULL;
+            temp=head;
+            continue;
+        }
+        else
+        {
+            temp->next= (struct node *) malloc( sizeof(struct node) );
+            temp=temp->next;
+            temp->data=value;
+            temp->next=NULL;
+        }
+    }
+}
+void printList(struct node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("
+");
+}
+/* Drier program to test above function*/
+int main(void)
+{
+    /* Start with the empty list */
+    int t,k,n,value;
+     /* Created Linked list is 1->2->3->4->5->6->7->8->9 */
+     scanf("%d",&t);
+     while(t--)
+     {
+     insert();
+     scanf("%d",&k);
+     rotate(&head,k);
+     printList(head);
+     }
+     return(0);
+}
+
+}
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/*
+  Rotate a linked list after node k
+  The input list will have at least one element
+  A Linked List node has following structure.
+  struct node
+  {
+     int data;
+     struct node *next;
+  }
+*/
+/*  **head_ref is a pointer to head pointer.  This function
+    should modify value at head_ref to change head (if required)*/
+void rotate(struct node **head_ref, int k)
+{
+   struct node *temp = *head_ref;
+   struct node *head,*prev;
+   int count = 0;
+   while(temp->next!=NULL){
+       count++;
+       if(count==k){
+           prev = temp;
+           head = temp->next;
+       }
+       temp=temp->next;
+   }
+   if(k<=count){
+       prev->next = NULL;
+       temp->next = (*head_ref);
+       *head_ref = head;
+   }
+
+}
